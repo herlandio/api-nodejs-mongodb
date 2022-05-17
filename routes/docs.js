0c087851a805ee6dd0b249b9e60cab8579b1f0bb
herlandio <herlandio7@gmail.com>
2022-05-17 15:16:25 -0300

    add docs

--- routes/docs.js
+++ routes/docs.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/api', function (req, res) {
+router.get('/api', function (req, res) {
     return res.send('ok');
 });
